6edaaa28d08ce58314853ccd829f40b2704d2c2d
Artem Zhonitsky <a.zhornickij@napoleonit.rua.zhornickij@napoleonit.rua.zhornickij@napoleonit.rua.zhornickij@napoleonit.rua.zhornickij@napoleonit.ru>
2020-08-05 14:37:12 +0500

    Initial Commit

---
+++ eptem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  eptem
+//
+//  Created by Artem Zhornitsky on 05.08.2020.
+//  Copyright © 2020 eptem. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
